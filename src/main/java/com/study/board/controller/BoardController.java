5a12fae4195e397a8e91455d3ceaa2ded8a89a40
myandue <97776790+myandue@users.noreply.github.com>
2022-11-18 00:02:20 +0900

    22.11.18

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,15 +1,24 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main(){
-        return "Hello~";
+    @GetMapping("/board/write")
+    public String boardWriteForm(){
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/write")
+    public String boardWritePro(Board board){
+        System.out.println("title = " + board.getTitle());
+        System.out.println("content = " + board.getContent());
+
+        return "";
     }
 }
---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,21 @@
+package com.study.board.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data //롬복 -> 외부에서 getter setter 로 접근할 수 있게하는듯
+public class Board {
+
+    @Id //pk 의미
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY -> mysql, mariadb / SEQUENCE -> Oracle
+    private Integer id;
+
+    private String title;
+
+    private String content;
+}
