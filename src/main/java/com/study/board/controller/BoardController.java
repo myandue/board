8cfc0d1442e75f896329daea255c96758342973b
myandue <97776790+myandue@users.noreply.github.com>
2022-11-20 17:02:36 +0900

    22.11.20

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,8 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,19 +19,60 @@ public class BoardController {
     @GetMapping("/board/write")
     public String boardWriteForm(){
 
-        return "boardwrite";
+        return "board-write";
     }
 
     @PostMapping("/board/write")
     public String boardWritePro(Board board){
-
+        //Board board 매개변수는 entity를 통해 param들을 통째로 가져오는 방식.
         boardService.write(board);
 
-        return "";
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/list")
-    public String boardList(){
-        return "boardlist";
+    public String boardList(Model model){
+
+        model.addAttribute("list",boardService.boardList());
+
+        return "board-list";
+    }
+
+    @GetMapping("/board/view/{id}") //localhost:8080/board/view/1
+    public String boardView(Model model, @PathVariable("id") Integer id){
+        model.addAttribute("article", boardService.boardView(id));
+
+        return "board-view";
+    }
+
+    @GetMapping("/board/modify/{id}") //localhost:8080/board/modify/1
+    public String boardModify(Model model, @PathVariable("id") Integer id){
+
+        model.addAttribute("article", boardService.boardView(id));
+
+        return "board-modify";
+    }
+
+    @PostMapping("/board/modify/{id}")
+    public String boardModifyPro(@PathVariable("id") Integer id, Board board){
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        //boardService.write(board);
+        //위처럼 작성해도 새롭게 작성되는게 아니라 수정이 반영되던데..?
+        return "redirect:/board/list";
+        //write 이랑 modify 상세페이지로 redirect 하고 싶음.
+    }
+
+    @GetMapping("/board/delete") //localhost:8080/board/delete?id=1
+    public String boardDelete(@RequestParam("id") Integer id){
+    //RequestParam 안의 텍스트와 html에 적힌 텍스트를 매칭시켜 바인딩 된다.
+    //해당 텍스트와 매개변수명이 같다면 어노테이션을 생략할 수 있다.
+
+        boardService.delete(id);
+
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,12 +5,35 @@ import com.study.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
 
+    //게시글 작성 처리
     public void write(Board board){
         boardRepository.save(board);
+    }
+
+    //게시글 리스트 처리
+    public List<Board> boardList(){
+        return boardRepository.findAll();
+    }
+
+    //특정 게시글 불러오기
+    public Board boardView(Integer id){
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 수정 처리
+    public Board boardModify(Integer id){
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제 처리
+    public void delete(Integer id){
+        boardRepository.deleteById(id);
     }
 }
