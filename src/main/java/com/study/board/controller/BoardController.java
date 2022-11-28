2017401e04be8c0dbda40f337c6ea3d5d4ab2356
myandue <97776790+myandue@users.noreply.github.com>
2022-11-28 20:32:59 +0900

    22.11.28

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -9,39 +9,37 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
 import java.util.Arrays;
 
 @Controller
+@RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping("/board/write")
-    public String boardWriteForm(){
+    @GetMapping("/write")
+    public String getArticleWrite(){
 
-        return "board-write";
+        return "article-write";
     }
 
-    @PostMapping("/board/write")
-    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
+    @PostMapping("/write")
+    public String postArticleWrite(Board board, Model model, MultipartFile file) throws Exception{
         //Board board 매개변수는 entity를 통해 param들을 통째로 가져오는 방식.
         //MultipartFile file은 뭐지,, -> input 중 name="file" 인 애를 받아오는 것
         boardService.write(board, file);
         model.addAttribute("message","글 작성이 완료되었습니다.");
-        model.addAttribute("searchUrl","view/"+board.getId()); //redirect할 주소 전달
+        model.addAttribute("searchUrl",""+board.getId()); //redirect할 주소 전달
 
         return "message"; //이러면 message라는 파일을 찾는거 아냐? 맞넼ㅋㅋㅋ 바본가
     }
 
-    @GetMapping("/board/list")
+    @GetMapping("/list")
     public String boardList(Model model,
                             @PageableDefault(page=0, size=20, sort="id", direction= Sort.Direction.DESC) Pageable pageable,
                             String searchKeyword){
@@ -68,34 +66,34 @@ public class BoardController {
         return "board-list";
     }
 
-    @GetMapping("/board/view/{id}") //localhost:8080/board/view/1
-    public String boardView(Model model, @PathVariable("id") Integer id){
+    @GetMapping("/{id}") //localhost:8080/board/1
+    public String articleDetail(Model model, @PathVariable("id") Integer id){
         model.addAttribute("article", boardService.boardView(id));
 
-        return "board-view";
+        return "article-detail";
     }
 
-    @GetMapping("/board/modify/{id}") //localhost:8080/board/modify/1
-    public String boardModify(Model model, @PathVariable("id") Integer id){
+    @GetMapping("/{id}/modify") //localhost:8080/board/1/modify
+    public String getArticleModify(Model model, @PathVariable("id") Integer id){
 
         model.addAttribute("article", boardService.boardView(id));
 
-        return "board-modify";
+        return "article-modify";
     }
 
-    @PostMapping("/board/modify/{id}")
-    public String boardModifyPro(Model model, Board board, MultipartFile file) throws Exception{
+    @PostMapping("/{id}/modify")
+    public String postArticleModify(Model model, Board board, MultipartFile file) throws Exception{
 
         boardService.write(board, file);
 
         model.addAttribute("message","글 수정이 완료되었습니다.");
-        model.addAttribute("searchUrl","/board/list");
+        model.addAttribute("searchUrl","");
         return "message";
         //write 이랑 modify 상세페이지로 redirect 하고 싶음.
     }
 
-    @GetMapping("/board/delete") //localhost:8080/board/delete?id=1
-    public String boardDelete(@RequestParam("id") Integer id){
+    @GetMapping("/delete") //localhost:8080/board/delete?id=1
+    public String articleDelete(@RequestParam("id") Integer id){
     //RequestParam 안의 텍스트와 html에 적힌 텍스트를 매칭시켜 바인딩 된다.
     //해당 텍스트와 매개변수명이 같다면 어노테이션을 생략할 수 있다.
 
